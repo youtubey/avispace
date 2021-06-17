a7210a427946eea1efdbb4595f31b3172b21188d
youtubey <70210187+youtubey@users.noreply.github.com>
2021-06-17 18:16:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'ODU1MDk1MTkzOTEzMzkzMTgy.YMtfdQ.s6IA9ZIm0gWpaMGJwMOAUk8-6i8',
+    'token': 'ODU1MDk1MTkzOTEzMzkzMTgy.YMtfdQ.dJUe5OqAfhUayfh021DElm-nw3M',
     'bot': 'AviSpace',
     'id': 855095193913393182,
     'prefix': '!'
